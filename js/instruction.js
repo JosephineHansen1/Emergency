2d52979cdafe64c9b9fefdf693cfde7120923cdd
Josephine <joha0014@stud.ek.dk>
2025-11-03 08:43:28 +0100

    indsatsvg

---
+++ js/instruction.js
@@ -0,0 +1,9 @@
+console.log("loaded instructions...");
+
+document.querySelector("by1").addEventListener("click", hhhhh);
+
+function byf() {
+  console.log("byf bla bla bla");
+  document.querySelector("#by1").classList.add("my_scale");
+  document.querySelector();
+}
